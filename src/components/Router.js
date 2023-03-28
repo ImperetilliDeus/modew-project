c41bfc9e1937540e13583a754e5165757944af03
하검 <87745742+ImperetilliDeus@users.noreply.github.com>
2023-03-28 18:44:28 +0900

    230328
    회원가입 기능 구현(firebase db에 저장)

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,29 +1,26 @@
-import React, { useState } from 'react';
-import {
-  HashRouter as Router, Route, Routes
-} from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
-import EditProfile from "../routes/EditProfile";
 import Home from "../routes/Home";
-import Profile from "../routes/Profile";
 
-
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({isLoggedIn}) => {
+  
   return (
     <Router>
-      <Routes>
-        <Route>
-          {isLoggedIn ? (
-            <>
-              <Route path='/' element={<Home/>} />
-            </>
-            ) : (
-              <Route path='/' element={<Auth/>} />
-          )}
-        </Route>
-      </Routes>
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
     </Router>
   );
-  }
+};
 export default AppRouter;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,37 +1,76 @@
-import React , { useState } from 'react';
-
+import React, { useState } from "react";
+import { authService } from "fbase";
 
 const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const onChange = (event) =>{
-        const {target:{name,value}}=event;
-        if (name==="email"){
-            setEmail(value)
-        } else if (name==="password"){
-            setPassword(value);
-        }
-    };
-    const onSubmit = (event) =>{
-        event.preventDefault();
-    };
 
-    return(
-        <div>
-            <h1>로그인페이지 예시</h1>
-            <form>
-                <input name="email" type="email" placeholder='Email' required value={email} onChange={onChange}/>
-                <br/>
-                <input name="password" type="password" placeholder='Password' required value={password} onChange={onChange}/>
-                <br/>
-                <input type="submit" value="Log In"/>
-            </form>
-            <br/>
-            <div>
-                <button>구글 계정으로 로그인</button>
-            </div>
-        </div>
-    )
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (event) => {
+
+    event.preventDefault();
+
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  return (
+    <div>
+        <h1>로그인 임시 페이지</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <br/>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <br/>
+        <input type="submit" value={newAccount ? "회원 가입" : "로그인"} />
+        <br/>
+        {error}
+      </form>
+      <div>
+        <button>구글 계정으로 로그인</button>
+      </div>
+    </div>
+  );
+
 }
-
-export default Auth
+export default Auth;
